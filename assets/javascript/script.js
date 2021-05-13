e2cbc88915c3b6b367b72012523ccb03266c7dbe
Jacob Molsby <jacobmolsby@hotmail.se>
2021-05-13 15:03:04 +0000

    Add multiplication game code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,7 +33,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }else {
         alert(`Uknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -50,8 +52,10 @@ function checkAnswer() {
 
     if(isCorrect) {
         alert("Hey! You got it correct!")
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -69,6 +73,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw`Unimplemented operator ${operator}. Aborting!`;
@@ -76,12 +82,20 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets the currect score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Get the corrent tally of incorrect answers from the DOM and iuncrements it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -94,7 +108,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 
 }
 
